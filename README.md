81a89b958873a18d9efc5a27240aa2417dc45ad9
macdressler <macsedressler@gmail.com>
2024-02-27 16:56:08 -0700

    I have messed around with the way the page looks and implemented some changes with the api

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ This is my submission for lab 3 in ENGO551
 
 I will use this list here to keep track of all of my different ChatGPT prompts.
 - I want to make a webpage using Flask, html and python that has a map on the first page. Can you generate me a python file with the backend for the page and an html file for the front end of this webpage?
+- how should I edit the html file so that the default location is Calgary Alberta?
+- I want the map to centre on calgary, without a marker.  Can you write code in html using leaflet in order to centre the map on Calgary alberta with a wide zoom?
+- The map now shows up. I need to add the search bars now, in order to be able to query an api for the date. Could you please create these two inputs in both the html and the python?
+- can you please centre the search, and make the whole page look nicer?
+- can you make it so that the map is always close to the bottom of the page on its bottom edge?
+- now the map is in the bottom left corner of the page. Can you make it centred horizontally at the bottom of the page?
--- main.py
+++ main.py
@@ -1,31 +1,26 @@
-import os
-import requests
-from flask import Flask, session, request, render_template, redirect, url_for, jsonify
-from flask_session import Session
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import scoped_session, sessionmaker
+from flask import Flask, render_template, request, jsonify
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app) # I need to make sure that I have a session going so that I can have active usernames. 
-
-# Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine)) # This loads the connection to my database
-
-
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('index.html') # This makes the search page the main page.
+    return render_template('index.html')
 
+@app.route('/search', methods=['POST'])
+def search():
+    start_date = request.form['start_date']
+    end_date = request.form['end_date']
+    
+    # Process the start_date and end_date as needed for your API request
+    # You can use the values in your API request to https://data.calgary.ca/resource/c2es-76ed.geojson
+
+    # For demonstration, let's just return the selected date range
+    return jsonify({'start_date': start_date, 'end_date': end_date})
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
     #In order to run the code type python -m flask run in the command line
