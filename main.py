f60d1e7adc3594d7707537bce502db337591aec5
macdressler <macsedressler@gmail.com>
2024-03-05 15:44:59 -0700

    messing around with the api

--- main.py
+++ main.py
@@ -11,24 +11,33 @@ def index():
 
 @app.route('/search', methods=['POST'])
 def search():
-    start_date = request.form['start_date']
-    end_date = request.form['end_date']
+    start_date = str(request.form['start_date'])
+    end_date = str(request.form['end_date'])
+    print(start_date, end_date)
     dictionary = []
-    res = requests.get('https://data.calgary.ca/resource/c2es-76ed.geojson?', params = {'issueddate' > start_date, 'issueddate' < end_date})
+    res = requests.get('https://data.calgary.ca/resource/c2es-76ed.geojson?', params = {"$where": 'issueddate' > start_date and 'issueddate' < end_date})
     info = res.json()
     if 'features' in info and info['features']:
         for item in info['features']:
+            print(item)
             data = item['geometry']
+            print(data)
             latitude = data.get('coordinates')[0]
-            longitude = data.get('coordinates')[1]
+            longitude = data.get('coordinates')[1]  
+            data1 = item['properties']
+            permit = data1.get('permitnum')
+            date = data1.get('issueddate')
+            name = data1.get('contractorname')
+            type = data1.get('permittype')
             if latitude and longitude:
-
-
+                dictionary.append({'latitude': latitude, 'longitude': longitude, 'permit':permit, 'date':date, 'name': name, 'type':type})
+                print(dictionary)
     
     # Process the start_date and end_date as needed for your API request
     # You can use the values in your API request to https://data.calgary.ca/resource/c2es-76ed.geojson
 
     # For demonstration, let's just return the selected date range
+    print(dictionary)
     return jsonify({'start_date': start_date, 'end_date': end_date})
 
 if __name__ == '__main__':
